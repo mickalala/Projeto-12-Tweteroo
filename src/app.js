20632b0705f0997a92fca97ddbb80c7b9502cd0b
mickalala <mickalarissa01@gmail.com>
2023-04-09 15:38:10 -0300

    Logando o usuario

--- src/app.js
+++ src/app.js
@@ -1,21 +1,37 @@
 import express from "express"
+import cors from "cors"
 
-const app =express();
+const app = express();
 
 app.use(express.json());
+app.use(cors());
 
-const users=[];
+const users = [];
 
-app.post("/sign-up",(req,res)=>{
-    const user=req.body;
+app.post("/sign-up", (req, res) => {
+    console.log(req)
+    const user = {
+        username: req.body.username,
+        avatar: req.body.avatar
+    }
     users.push(user)
     res.send("OK")
 })
 
-app.post("/tweets",(req,res)=>{
-    
+app.post("/tweets", (req, res) => {
+
 })
 
-const PORT=5000
+const tweets=[	{
+    username: "bobesponja",
+    avatar: "https://cdn.shopify.com/s/files/1/0150/0643/3380/files/Screen_Shot_2019-07-01_at_11.35.42_AM_370x230@2x.png",
+    tweet: "Eu amo hambúrguer de siri!"
+}];
+
+app.get("/tweets",(req,res)=>{
+    res.send(tweets)
+})
+
+const PORT = 5000
 
 app.listen(PORT, console.log(`tá rodando na porta ${PORT}`))
