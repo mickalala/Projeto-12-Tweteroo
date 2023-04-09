b6c4b5b6ce716ce4b3d8fefb1eb087ec45e5f01b
mickalala <mickalarissa01@gmail.com>
2023-04-09 17:15:44 -0300

    Carregando tweets

--- src/app.js
+++ src/app.js
@@ -8,25 +8,33 @@ app.use(cors());
 
 const users = [];
 
+const tweets=[];
+
+let userAvatar;
+
 app.post("/sign-up", (req, res) => {
-    console.log(req)
+
     const user = {
         username: req.body.username,
         avatar: req.body.avatar
     }
+    userAvatar=req.body.avatar;
     users.push(user)
     res.send("OK")
 })
 
 app.post("/tweets", (req, res) => {
-
+    if(!users.includes(req.body.username)){
+        res.send("UNAUTHORIZED")
+    }
+     const tweet={
+        username: req.body.username,
+        avatar:userAvatar,
+        tweet: req.body.tweet
+     }
+     tweets.push(tweet)
+    res.send("OK")
 })
-
-const tweets=[	{
-    username: "bobesponja",
-    avatar: "https://cdn.shopify.com/s/files/1/0150/0643/3380/files/Screen_Shot_2019-07-01_at_11.35.42_AM_370x230@2x.png",
-    tweet: "Eu amo hambúrguer de siri!"
-}];
 
 app.get("/tweets",(req,res)=>{
     res.send(tweets)
